fffc82ee60d61d94fa3f982406185e380f0f8d3e
DivyanshKG <diviirockgod6@gmail.com>
2024-09-01 14:39:39 +0530

    worked on it some more

--- OS-simpleloader/loader.c
+++ OS-simpleloader/loader.c
@@ -25,7 +25,7 @@ void loader_cleanup() {
 }
 
 void* memory_mapping(void* address, size_t size, int prot, int flags ){
-    void* memory = mmap(address, size, flags, prot, fd, offset);
+    void* memory = mmap(address, size, prot, flags, -1, 0);
     if(memory==MAP_FAILED){
       perror("Error thrown in mapping memory");
       exit(1);
@@ -41,12 +41,9 @@ void readfile(void* addr, size_t size){
       exit(1);
   }
 }
-
-/*
- * Load and run the ELF executable file
- */
-void load_and_run_elf(char** exe) {
-  fd = open(argv[1], O_RDONLY);
+  // 1. Load entire binary content into the memory from the ELF file.
+  void load_and_run_elf(char* elf) {
+  fd = open(elf, O_RDONLY);
   if(fd==-1){
     perror("Error");
     exit(1);
@@ -69,18 +66,30 @@ void load_and_run_elf(char** exe) {
     exit(1);
   }
   readfile(phdr, sizeof(Elf32_Phdr)* ehdr->e_phnum);
-
+// 2. Iterate through the PHDR table and find the section of PT_LOAD 
+//    type that contains the address of the entrypoint method in fib.c
   for( int i = 0; i<ehdr->e_phnum, i++){
     if(phdr[i].p_type == PT_LOAD)
   }
-
-  // 1. Load entire binary content into the memory from the ELF file.
-  // 2. Iterate through the PHDR table and find the section of PT_LOAD 
-  //    type that contains the address of the entrypoint method in fib.c
+  
   // 3. Allocate memory of the size "p_memsz" using mmap function 
   //    and then copy the segment content
+  void *memory = memory_mapping((void *)phdr[i].p_vaddr, phdr[i].p_memsz, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS);
   // 4. Navigate to the entrypoint address into the segment loaded in the memory in above step
+  while(0){
+    lseek(fd, phdr[i].p_offset, SEEK_SET);
+    ssize_t bytes_read = read(fd, memory, phdr[i].p_filesz);
+    if (bytes_read == -1) {
+        perror("Error while reading data into memory");
+        munmap(memes, phdr[i].p_memsz);
+        exit(1);
+  }
+  if (mprotect(memes, phdr[i].p_memsz, PROT_READ | PROT_EXEC) == -1) {
+      perror("Error adjusting memory protection");
+      munmap(memes, phdr[i].p_memsz); // Clean up the mmap
+      exit(1);
   // 5. Typecast the address to that of function pointer matching "_start" method in fib.c.
+  int (*start_)(void) = (int (*)(void))(ehdr->e_entry);
   // 6. Call the "_start" method and print the value returned from the "_start"
   int result = _start();
   printf("User _start return value = %d\n",result);
