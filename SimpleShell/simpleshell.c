1600e35236f4b4a46e0ae664c56fe5d5b0f1a203
Ishaaann <ishaan.raj1611@gmail.com>
2024-09-23 23:04:30 +0530

    Completed the main and launch function

--- SimpleShell/simpleshell.c
+++ SimpleShell/simpleshell.c
@@ -9,14 +9,38 @@
 #include <time.h> // Time-related functions
 #include <ctype.h> 
 
+int launch(char* cmd){
+    int status = create_process_and_run(cmd);
+    return status;
+}
+
+void my_handler(int signum){
+    if(signum==SIGINT){
+        SIGINT_history();
+    }
+}
+
+
 int main(){
+    struct sigaction sig;
+    memset(&sig,0,sizeof(sig));
+    sig.sa_handler = my_handler;
+    sigaction(SIGINT, &sig, NULL);
 
     do{
-        printf("ishaan&&divyansh@simpleShell:~$ ");
-        if(fgets(input,sizeof(input),stdin)==NULL){
-            perror("")
+        printf("ishaan&&divyansh@SimpleShell:$ ");
+        char* cmd = readUserInput();
+
+        if(cmd!=NULL){
+            int status = launch(cmd);
+            free(cmd);
+
+            if(cmd!=0){
+                printf("Command exited with status %d\n", status);  
+            }
         }
     }
+    while(true); //infinte loop until loop is broken with CTRL+C
 
     return 0;
 }
