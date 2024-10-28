14efa73a04538632ff1c3a4400662dfa424b7acf
Divyansh Kumar Gautam <86947567+DivyanshKG@users.noreply.github.com>
2024-10-28 16:11:35 +0530

    Create p4.c

---
+++ SimpleScheduler/p4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "dummy_main.h"
+
+void print_hello_world(){
+  printf("hello world");
+}
+
+int main(int argc, char **argv) {
+    print_hello_world();
+    return 0;
+}
