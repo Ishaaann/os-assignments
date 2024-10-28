b1a06b664006b333c098ffff2cb37d4ebe0f672e
Divyansh Kumar Gautam <86947567+DivyanshKG@users.noreply.github.com>
2024-10-28 16:10:20 +0530

    Create p2.c

---
+++ SimpleScheduler/p2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "dummy_main.h"
+
+void print_hello_divyansh(int n) {
+    printf("Hello Divyansh");
+}
+
+int main(int argc, char **argv) {
+    print_hello_divyansh();
+    return 0;
+}
